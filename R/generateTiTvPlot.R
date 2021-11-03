1360cff1e385e418bf93647a9588e07b2c61e46d
Ashish Jain <jain.ashishjain1@gmail.com>
2021-11-03 11:34:40 -0400

    Added skeleton for the TiTv plot

---
+++ R/generateTiTvPlot.R
@@ -0,0 +1,16 @@
+#' Function to generate the frequency of Transitions and Transversions of gene mutations
+#' @description This function generate the frequency of Transitions and Transversions of gene mutations
+#' @author Mayank Tandon, Ashish Jain
+#' @param maf A MAF object
+#' @export
+#' @return No return value. If 'save_name' is not provided, then the plot is printed to the current graphics device, otherwise a PDF is created at the given path.
+#'
+#' @examples
+#' library(MAFDash)
+#' library(maftools)
+#' maf <- system.file("extdata", "test.mutect2.maf.gz", package = "MAFDash")
+#'
+generateTiTvPlot<-function(maf,save_name=NULL){
+
+
+}
