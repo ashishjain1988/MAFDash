c7dac287b2233270975f8b29c336cfaba800ee5a
Ashish Jain <jain.ashishjain1@gmail.com>
2021-11-08 16:24:03 -0500

    Updated the TiTv plot function

--- R/generateTiTvPlot.R
+++ R/generateTiTvPlot.R
@@ -6,15 +6,15 @@ utils::globalVariables(c("value","variable","V1"))
 #' @author Mayank Tandon, Ashish Jain
 #' @param maf A MAF object
 #' @param use_silent_mutations Include synonymous variants in analysis. Defaults to FALSE.
-#' @param sampleOrder Sample names in which the barplot should be ordered. Default NULL
+#' @param sampleOrder Sample names in which the barplot should be ordered. Default NULL.
 #' @param color named vector of colors for each coversion class.
-#' @param showBarcodes Whether to include sample names for barplot
-#' @param textSize fontsize if showBarcodes is TRUE. Deafult 2.
-#' @param baseFontSize font size. Deafult 1.
-#' @param axisTextSize text size x and y tick labels. Default c(1,1).
-#' @param save_name The name and path of the output file
+#' @param showBarcodes Whether to include sample names for barplot.
+#' @param textSize fontsize if showBarcodes is TRUE. Default 15 (in pts).
+#' @param baseFontSize font size of axis title. Default 15 (in pts).
+#' @param axisTextSize text size x and y tick labels. Default 10 (in pts).
+#' @param save_name The name and path of the output file. Default NULL.
 #' @export
-#' @return No return value. If 'save_name' is not provided, then the plot is printed to the current graphics device, otherwise a PDF is created at the given path.
+#' @return List of objects consisting of different TiTv plots. If 'save_name' is provided, a PDF is created at the given path.
 #'
 #' @examples
 #' library(MAFDash)
@@ -22,9 +22,12 @@ utils::globalVariables(c("value","variable","V1"))
 #' maf <- system.file("extdata", "test.mutect2.maf.gz", package = "MAFDash")
 #' plots<-generateTiTvPlot(read.maf(maf))
 #'
+#' @importFrom tidyr pivot_longer
+#' @importFrom cowplot plot_grid
+#'
 generateTiTvPlot<-function(maf,use_silent_mutations = FALSE, sampleOrder = NULL,
-                           color = NULL, showBarcodes = FALSE, textSize = 0.8, baseFontSize = 1,
-                           axisTextSize = c(1, 1),save_name=NULL){
+                           color = NULL, showBarcodes = FALSE, textSize = 15, baseFontSize = 15,
+                           axisTextSize = 10,save_name=NULL){
 
   maf <- ensurer::ensure_that(maf,!is.null(.) && (class(.) == "MAF"),
                               err_desc = "Please enter correct MAF object")
@@ -60,12 +63,13 @@ generateTiTvPlot<-function(maf,use_silent_mutations = FALSE, sampleOrder = NULL,
   orderlvl = as.character(titv.order$variable)
   titv.frac.melt$variable = factor(x = titv.frac.melt$variable, levels = rev(orderlvl))
 
-  # tf = tiTvResults$TiTv.fractions
-  # data.table::setDF(x = tf)
-  # rownames(tf) = tf$Tumor_Sample_Barcode
-  # tf = tf[,-1]
+  tf = tiTvResults$TiTv.fractions
+  titv.melt = tf %>% tidyr::pivot_longer(!Tumor_Sample_Barcode, names_to = "variable", values_to = "value")
 
-  # if(plotType == 'bar'){
+  if(!is.null(sampleOrder))
+  {
+    titv.frac.melt$Tumor_Sample_Barcode = factor(x = titv.frac.melt$Tumor_Sample_Barcode, levels = sampleOrder)
+  }
 
   ##TODO: Make the plot look better and increase font sizes
   barplot <- ggplot2::ggplot(titv.frac.melt,aes(x=Tumor_Sample_Barcode,y=value, fill=variable)) +
@@ -74,27 +78,42 @@ generateTiTvPlot<-function(maf,use_silent_mutations = FALSE, sampleOrder = NULL,
     ggplot2::scale_y_continuous(expand = c(0, 0),limits = c(0,100.2)) +
     ggplot2::xlab("") + ggplot2::ylab("% of Mutations") +
     ggplot2::guides(fill = guide_legend(title = "TiTv")) +
-    ggplot2::theme_classic(base_size = 10)
+    ggplot2::theme_classic(base_size = 10) +
+    ggplot2::theme(text=element_text(face = "bold"),axis.text = element_text(size = axisTextSize,colour = "black"),
+                   axis.title = element_text(size = baseFontSize,face = "bold"),legend.background = element_rect(colour = "black")) +
+    ggplot2::theme(axis.title.x = element_text(size = textSize,face = "bold"))
 
   if(!showBarcodes)
   {
     barplot <- barplot + ggplot2::theme(axis.text.x = element_blank(),axis.ticks.x = element_blank())
   }
-  # } else if(plotType == 'box'){
 
   titv.frac.melt$variable = factor(x = titv.frac.melt$variable, levels = orderlvl)
   ##TODO: Make the plot look better and increase font sizes
   boxplot<-ggplot2::ggplot(titv.frac.melt,aes(x=variable,y=value,fill = variable))+
     ggplot2::theme_classic(base_size = 10) +
-    #ggplot2::theme(text=element_text(face = "bold"),axis.text = element_text(size = 20,colour = "black"),axis.title = element_text(size = 20,face = "bold"),legend.background = element_rect(colour = "black")) +
     ggplot2::geom_boxplot(outlier.colour="black", outlier.shape=16,outlier.size=1, notch=FALSE)+
     scale_fill_manual(values=col) +
-    #ggplot2::guides(fill = guide_legend(title = "TiTv")) +
-    #ggplot2::geom_point(size=1.5)+
+    ggplot2::theme(text=element_text(face = "bold"),axis.text = element_text(size = axisTextSize,colour = "black"),
+                   axis.title = element_text(size = baseFontSize,face = "bold"),legend.background = element_rect(colour = "black")) +
     ggplot2::guides(fill="none") +
     ggplot2::labs(x="", y = "% of Mutations")
-  #ggplot2::geom_hline(yintercept=-1, linetype="dashed", color = "red") +
-  #theme(plot.margin = unit(c(0.5,1,0.5,0.5), "cm"))
 
-  return(list(boxplot=boxplot,barplot=barplot))
+  ##TODO: Make the plot look better and increase font sizes
+  boxplot1<-ggplot2::ggplot(titv.melt,aes(x=variable,y=value,fill = variable))+
+    ggplot2::theme_classic(base_size = 10) +
+    #ggplot2::theme(text=element_text(face = "bold"),axis.text = element_text(size = 20,colour = "black"),axis.title = element_text(size = 20,face = "bold"),legend.background = element_rect(colour = "black")) +
+    ggplot2::geom_boxplot(outlier.colour="grey", outlier.shape=16,outlier.size=1, notch=FALSE)+
+    scale_fill_manual(values=col) +
+    ggplot2::guides(fill="none") +
+    ggplot2::labs(x="", y = "% of Mutations") +
+    ggplot2::theme(text=element_text(face = "bold"),axis.text = element_text(size = axisTextSize,colour = "black"),
+                   axis.title = element_text(size = baseFontSize,face = "bold"),legend.background = element_rect(colour = "black"))
+
+  if(!is.null(save_name))
+  {
+    combinePlot <- cowplot::plot_grid(cowplot::plot_grid(boxplot,boxplot1,ncol = 2,rel_widths = c(2,1)),barplot,nrow = 2)
+    ggsave(save_name,combinePlot)
+  }
+  return(list(tiTvPatterns=boxplot,TiTv = boxplot1,barplot=barplot))
 }
